d2a8b1c7a23fc3c234345d04b89dc3bf5a66ed6e
annismiles <ana.katsitadze.1@btu.edu.ge>
2021-02-04 23:15:06 +0400

    Second Commit

--- app/src/main/java/com/example/chatify/MainActivity.kt
+++ app/src/main/java/com/example/chatify/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.chatify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var registerButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerButton = findViewById(R.id.registrationButton)
+
+        registerButton.setOnClickListener {
+            startActivity(Intent(this,RegistrationActivity::class.java))
+        }
     }
 }
