12b1e5dc13893c1dc9df1a427a3ddd9cd4df0e0d
annismiles <ana.katsitadze.1@btu.edu.ge>
2021-02-05 00:33:16 +0400

    Third Commit

--- app/src/main/java/com/example/chatify/MainActivity.kt
+++ app/src/main/java/com/example/chatify/MainActivity.kt
@@ -7,13 +7,20 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var registerButton: Button
+    private lateinit var sign: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         registerButton = findViewById(R.id.registrationButton)
+        sign = findViewById(R.id.signupButton)
 
         registerButton.setOnClickListener {
             startActivity(Intent(this,RegistrationActivity::class.java))
         }
+        sign.setOnClickListener {
+            startActivity(Intent(this,NavigationActivity::class.java))
+        }
+
+
     }
 }
